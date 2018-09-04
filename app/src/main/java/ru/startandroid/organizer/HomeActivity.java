431d6dedde069bd9572883209710f0ab5420aad7
LAPTOP-6RLAB1EJ\MauzerTheCat <mauzerthecat@gmail.com>
2018-09-05 00:05:18 +0200

    home fragment was added

--- app/src/main/java/ru/startandroid/organizer/HomeActivity.java
+++ app/src/main/java/ru/startandroid/organizer/HomeActivity.java
@@ -1,14 +1,20 @@
 package ru.startandroid.organizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    FragmentTransaction transaction;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        transaction = getSupportFragmentManager ().beginTransaction ();
+        transaction.replace (R.id.activity_catalog_fragment_cont, HomeFragment.newInstance ());
+        transaction.commit ();
     }
 }
