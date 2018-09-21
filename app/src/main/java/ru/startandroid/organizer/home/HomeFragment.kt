183551c93f616f4cf76d49beba912d37c7ab97c0
Dmitry Pliskov <dmitry.vinogradov@axelspringer.com>
2018-09-21 15:55:28 +0200

    [task-4] clean and fixes

--- app/src/main/java/ru/startandroid/organizer/home/HomeFragment.kt
+++ app/src/main/java/ru/startandroid/organizer/home/HomeFragment.kt
@@ -6,11 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.IntegerRes
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.fragment_home.*
 import dagger.android.AndroidInjection
 import ru.startandroid.data.AppDatabase
 import ru.startandroid.organizer.R
