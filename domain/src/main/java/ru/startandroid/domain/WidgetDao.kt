0bb47027fef024b92f136c269fbe07847b966a74
LAPTOP-6RLAB1EJ\MauzerTheCat <mauzerthecat@gmail.com>
2018-09-30 11:51:29 +0200

    [task-11] Try to use Room gradle troubles

--- domain/src/main/java/ru/startandroid/domain/WidgetDao.kt
+++ domain/src/main/java/ru/startandroid/domain/WidgetDao.kt
@@ -1,12 +1,24 @@
 package ru.startandroid.domain
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
+import io.reactivex.Flowable
+
 
 @Dao
 interface WidgetDao {
 
     @Query("SELECT * FROM widgets")
-    fun getAll(): LiveData<List<WidgetEntity>>
+    fun getAll(): Flowable<List<WidgetEntity>>
+
+    @Query("DELETE FROM widgets")
+    fun deleteAll()
+
+    @Insert
+    fun insert(widgetEntity: WidgetEntity)
+
+    @Insert
+    fun insert(list: List<WidgetEntity>)
 
 }
