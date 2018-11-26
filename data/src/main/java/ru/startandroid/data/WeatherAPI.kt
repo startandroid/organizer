b7ad7a874cb2cae49d30b01b72b5720fa1730190
LAPTOP-6RLAB1EJ\MauzerTheCat <mauzerthecat@gmail.com>
2018-11-26 22:18:34 +0200

    [task-15] Use retrofit to create RestApi for Weather API ready for review - gradle fixed

--- data/src/main/java/ru/startandroid/data/WeatherAPI.kt
+++ data/src/main/java/ru/startandroid/data/WeatherAPI.kt
@@ -1,4 +1,15 @@
 package ru.startandroid.data
 
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import ru.startandroid.domain.models.WeatherData
+
+
 interface WeatherAPI {
+
+    @GET("q={city}&days=7")
+    fun getCityWeather(@Query("city") city: String): Call<WeatherData>
+
 }
