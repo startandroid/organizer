a1d3804d06fa7a2be9ccfe592c5dc503c4b57d7b
Dmitry Vinogradov <startofandroid@gmail.com>
2019-07-07 23:47:17 +0200

    [task-60] Create fragments for widget config screen

--- app/src/main/java/ru/startandroid/organizer/TestActivity.kt
+++ app/src/main/java/ru/startandroid/organizer/TestActivity.kt
@@ -1,6 +1,7 @@
 package ru.startandroid.organizer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dagger.android.AndroidInjection
 
@@ -9,6 +10,24 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+        Log.d("qweee", "act onCreate")
         setContentView(R.layout.activity_test)
+
+        if (savedInstanceState == null) {
+            Log.d("qweee", "act add frag")
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.cont, WidgetConfigContainerFragment.newInstance(3))
+                    .commit()
+        }
+
+        onBackPressedDispatcher.addCallback {
+            (supportFragmentManager.findFragmentById(R.id.cont) as? WidgetConfigContainerFragment)
+                    ?.let {
+                        it.onBackPressed()
+                    } ?: false
+        }
+
     }
+
 }
